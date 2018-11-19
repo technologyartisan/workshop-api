d29876d4eb5248906ffa5162735aba9ad7510137
Urip Yogantara <uripyogantara@gmail.com>
2018-11-19 10:48:58 +0800

    push notification

---
+++ app/Http/Controllers/NotificationController.php
@@ -0,0 +1,57 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Validator;
+
+class NotificationController extends Controller
+{
+    public function sendNotification(Request $request){
+
+        $validator=Validator::make($request->all(),[
+            'title' => 'required',
+            'fcm_token' => 'required',
+            'message' => 'required'
+        ]);
+
+        if ($validator->fails()) {
+            return response()->json([
+                'success' => false,
+                'message' => 'Mohon Lengkapi Data'
+            ],500);
+        }
+
+        $title=$request->title;
+        $key = 'AIzaSyAsCpI_EBtJI-MOqEcOafzR_gwawNh4_00';
+        $fcmUrl = 'https://fcm.googleapis.com/fcm/send';
+
+        
+        $token=$request->fcm_token;
+        $headers = array(
+            'Authorization: key='.$key,
+            'Content-Type: application/json'
+        );
+        
+        $fields = array(
+            'to' => $token,
+            'notification' => array(
+                'title' => $title,
+                'body' => $request->message,
+                'sound'=>'default'
+            )
+        );
+        
+        $curl_session = curl_init();
+        curl_setopt($curl_session, CURLOPT_URL,$fcmUrl);
+        curl_setopt($curl_session, CURLOPT_POST, true);
+        curl_setopt($curl_session, CURLOPT_HTTPHEADER, $headers);
+        curl_setopt($curl_session, CURLOPT_RETURNTRANSFER, true);
+        curl_setopt($curl_session, CURLOPT_SSL_VERIFYPEER, false);
+        curl_setopt($curl_session, CURLOPT_POSTFIELDS, json_encode($fields));
+        $result = curl_exec($curl_session);
+        curl_close($curl_session);
+
+        return response()->json(['success' => 'Pesan sudah terkirim']);
+    }
+}
--- routes/api.php
+++ routes/api.php
@@ -17,3 +17,6 @@ Route::resource('contact', 'ContactController');
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+Route::post('sendNotification','NotificationController@sendNotification');
+
